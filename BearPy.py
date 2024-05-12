98dec0ae91701becb9da4480a3b4057c77ad6e75
alone1323e <92257300+alone1323e@users.noreply.github.com>
2024-05-11 21:45:50 -0300

    BearPy.py

---
+++ BearPy.py
@@ -0,0 +1,21 @@
+#def numput (num1, num2):
+#    num1 = int(input())
+#    num2 = int(input())
+#    num3 = num1 + num2
+#    print(numput(num1, num2))
+#    return print('funcionando')
+from random import randint
+
+#def numput (num1, num2):
+#    num1 = int(input())
+#    num2 = int(input())
+#    num3 = num1 + num2
+#  print(numput(num1, num2))
+#    return print('funcionando')
+
+def ranum (n):
+    for n in range(12):
+        print(randint(0,200))
+
+
+print(ranum(1))
